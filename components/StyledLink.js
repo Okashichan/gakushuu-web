bbda11b532aafa65676decfa527f719b1b9687d8
Klymchuk Bohdan <ipzk211_kba@student.ztu.edu.ua>
2024-06-03 04:46:14 +0300

    fixed caching on user page

--- components/StyledLink.js
+++ components/StyledLink.js
@@ -1,12 +1,18 @@
-import Link from "next/link";
+import { useRouter } from 'next/navigation';
 import { Link as MuiLink } from "@mui/material";
 
 export default function StyledLink({ href, children }) {
+
+    const router = useRouter();
+
+    const handleClick = (href) => {
+        router.push(href);
+        router.refresh();
+    };
+
     return (
-        <Link href={href} passHref legacyBehavior>
-            <MuiLink underline="none" color='inherit'>
-                {children}
-            </MuiLink>
-        </Link>
+        <MuiLink underline="none" color='inherit' onClick={() => handleClick(href)}>
+            {children}
+        </MuiLink>
     );
 }
