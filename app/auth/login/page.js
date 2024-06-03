8560640f8803b8f4fabbf9e60dec7528e50a0e40
Klymchuk Bohdan <ipzk211_kba@student.ztu.edu.ua>
2024-06-03 04:01:02 +0300

    deployment fixes #3

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
@@ -10,12 +9,14 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { unstable_noStore as noStore } from 'next/cache'
 
 import { setCookie } from '@/utils/cookies';
 
-const url = process.env.NEXT_PUBLIC_API_URL + '/token';
-
 export default function SignIn() {
+    noStore();
+    const url = process.env.NEXT_PUBLIC_API_URL + '/token';
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
